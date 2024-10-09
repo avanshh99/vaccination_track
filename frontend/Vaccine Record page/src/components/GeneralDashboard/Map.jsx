6e2cbb346f7b93f56b1e9a3fea7ed6fc175c844e
Dev Gaonkar <devpgaonkar3@gmail.com>
2024-10-09 19:13:36 +0530

    hover

--- frontend/Vaccine Record page/src/components/GeneralDashboard/Map.jsx
+++ frontend/Vaccine Record page/src/components/GeneralDashboard/Map.jsx
@@ -7,18 +7,16 @@ function Map() {
       {/* Left Side - Text and Button */}
       <div className="md:w-1/2 px-6 space-y-4 text-left">
         <h1 className="text-4xl font-bold text-black">
-          Historic & Unparalleled Achievement!
-          <br />
-          India’s Glorious Journey of
+          Vaccinations at your fingertips
         </h1>
         <div className="border-2 border-orange-500 rounded-md px-4 py-2 inline-flex items-center space-x-2">
           <span className="text-green-600 text-2xl">✔</span>
-          <span className="text-orange-600 font-bold text-2xl">200 CRORE VACCINATIONS</span>
+          <span className="text-orange-600 font-bold text-2xl">200 Crore vaccinations</span>
         </div>
         <p className="text-black italic">
-          "India is set to defeat Covid-19. Every Indian is making it possible."
+          Easily manage your vaccination records. Get reminders for upcoming appointments and stay informed about the latest vaccine recommendations
           <br />
-          <span className="font-bold">– PM Narendra Modi</span>
+          {/* <span className="font-bold">– PM Narendra Modi</span> */}
         </p>
       </div>
 
